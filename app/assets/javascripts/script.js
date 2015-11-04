8b8bf561be17598c6c9c178bec67daf5861ac688
Owen Jow <owenjow@berkeley.edu>
2015-11-04 03:33:48 -0800

    Eternalized the banana button; yes this is very important

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    $('#bananafy-button').click(function() {
+function enableBananafication() {
+    $('#bananafy-button').on('click', function(evt) {
         // Change a bunch of CSS attributes
         $('body').css({
             'background': 'url(/assets/bananas.jpg)',
@@ -7,8 +7,12 @@ $(document).ready(function() {
         });
         
         $('#header-bar').css('opacity', 0.3);
+        $('#footer-bar').css('opacity', 0.5);
         $('a:link').css('color', '#ffffff');
         $('a:visited').css('color', '#ffffff');
         $('.footer-overline').css('background', '#ffffff');
     });
-});
+}
+
+$(document).ready(enableBananafication);
+$(document).on('page:load', enableBananafication);
