1d19567b8eaf58f84d388ca269bc35e180c203ef
Owen Jow <owenjow@berkeley.edu>
2015-11-06 02:20:11 -0800

    Added seed data and user-related CRUD pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users
   root to: 'pages#index'
   get 'expenses', to: 'pages#expenses'
   
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+purchases = (1..6).to_a.map do |n|
+  rand_date = Time.at(rand * Time.now.to_i)
+  rand_cost = rand(1..100)
+  
+  Purchase.create! date_purchased: rand_date, cost: rand_cost
+end
+
+%w(Owen Sagang Jerry Oliver Kavi).each do |name|
+  user = User.create! username: name, email: name + "@berkeley.edu", password: 'abcd1234', weekly_budget: 50.00
+  user.expenses.create purchase: purchases[rand(purchases.size)]
+end
