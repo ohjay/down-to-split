740ac846cac988e855758ab3e5386b14231041f3
Owen Jow <owenjow@berkeley.edu>
2016-01-20 21:49:35 -0800

    Fixed many-to-many relationship

--- app/models/shopping_trip.rb
+++ app/models/shopping_trip.rb
@@ -1,6 +1,6 @@
 class ShoppingTrip < ActiveRecord::Base
 	belongs_to :vendor
 	has_many :purchases 
-	has_many :users
+	has_and_belongs_to_many :users
 	accepts_nested_attributes_for :purchases
 end
