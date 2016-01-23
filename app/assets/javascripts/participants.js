7060df3d04f80dfa8377006c14bf23cb28f29cce
Owen Jow <owenjow@berkeley.edu>
2016-01-22 23:08:41 -0800

    Dealt with lagomorphic and text duplication issues

--- app/assets/javascripts/participants.js
+++ app/assets/javascripts/participants.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
     $("input#add-participant").unbind("click").click(function() {
-        if ($("input#usearch-box").val()) {
-            $('#pform-list .participant-input').clone().appendTo('#pform-list');
+        if ($("input#usearch-box:last").val()) {
+            $('#pform-list .participant-input:first').clone().appendTo('#pform-list').find('input#usearch-box').val('');
             $('#pform-error').html('<br/>');
         } else {
             $('#pform-error').html('<br/>You already have input boxes available!');
         }
-        
-        console.log(document.getElementById("usearch-box").value);
     });
 });
