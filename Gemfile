8d59f50ad91d0c316ee55065e93fce2a49f33ff2
Sagang Wee <sagangwee@yahoo.com>
2016-04-08 06:21:55 +0000

    postgres

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ ruby '2.2.4'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
 # Use postgresql as the database for Active Record
-gem 'activerecord-jdbcpostgresql-adapter'
+# gem 'activerecord-jdbcpostgresql-adapter'
 gem 'rails_12factor', group: :production
 # Use sqlite3 as the database for Active Record
 # gem 'sqlite3'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160408025917) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "debts", force: :cascade do |t|
     t.float    "cost"
     t.datetime "created_at",       null: false
@@ -23,8 +26,8 @@ ActiveRecord::Schema.define(version: 20160408025917) do
     t.integer  "purchase_id"
   end
 
-  add_index "debts", ["purchase_id"], name: "index_debts_on_purchase_id"
-  add_index "debts", ["shopping_trip_id"], name: "index_debts_on_shopping_trip_id"
+  add_index "debts", ["purchase_id"], name: "index_debts_on_purchase_id", using: :btree
+  add_index "debts", ["shopping_trip_id"], name: "index_debts_on_shopping_trip_id", using: :btree
 
   create_table "expenses", force: :cascade do |t|
     t.integer  "user_id"
@@ -34,8 +37,8 @@ ActiveRecord::Schema.define(version: 20160408025917) do
     t.datetime "updated_at",  null: false
   end
 
-  add_index "expenses", ["purchase_id"], name: "index_expenses_on_purchase_id"
-  add_index "expenses", ["user_id"], name: "index_expenses_on_user_id"
+  add_index "expenses", ["purchase_id"], name: "index_expenses_on_purchase_id", using: :btree
+  add_index "expenses", ["user_id"], name: "index_expenses_on_user_id", using: :btree
 
   create_table "products", force: :cascade do |t|
     t.string   "product_name"
@@ -54,7 +57,7 @@ ActiveRecord::Schema.define(version: 20160408025917) do
     t.integer  "vendor_id"
   end
 
-  add_index "purchases", ["shopping_trip_id"], name: "index_purchases_on_shopping_trip_id"
+  add_index "purchases", ["shopping_trip_id"], name: "index_purchases_on_shopping_trip_id", using: :btree
 
   create_table "shopping_trips", force: :cascade do |t|
     t.datetime "created_at",               null: false
@@ -67,15 +70,15 @@ ActiveRecord::Schema.define(version: 20160408025917) do
     t.date     "date"
   end
 
-  add_index "shopping_trips", ["vendor_id"], name: "index_shopping_trips_on_vendor_id"
+  add_index "shopping_trips", ["vendor_id"], name: "index_shopping_trips_on_vendor_id", using: :btree
 
   create_table "shopping_trips_users", id: false, force: :cascade do |t|
     t.integer "shopping_trip_id"
     t.integer "user_id"
   end
 
-  add_index "shopping_trips_users", ["shopping_trip_id"], name: "index_shopping_trips_users_on_shopping_trip_id"
-  add_index "shopping_trips_users", ["user_id"], name: "index_shopping_trips_users_on_user_id"
+  add_index "shopping_trips_users", ["shopping_trip_id"], name: "index_shopping_trips_users_on_shopping_trip_id", using: :btree
+  add_index "shopping_trips_users", ["user_id"], name: "index_shopping_trips_users_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -98,9 +101,9 @@ ActiveRecord::Schema.define(version: 20160408025917) do
     t.integer  "debtor_id"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["username"], name: "index_users_on_username", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
   create_table "vendors", force: :cascade do |t|
     t.string   "vendor_name"
@@ -109,4 +112,8 @@ ActiveRecord::Schema.define(version: 20160408025917) do
     t.datetime "updated_at",  null: false
   end
 
+  add_foreign_key "debts", "purchases"
+  add_foreign_key "debts", "shopping_trips"
+  add_foreign_key "purchases", "shopping_trips"
+  add_foreign_key "shopping_trips", "vendors"
 end
